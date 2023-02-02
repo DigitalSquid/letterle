03a6fc0cc54b385c5023fe5e4d22b0adff8a86a6
Kevin Robertson <kevin.robertson13@gmail.com>
2023-02-02 18:06:31 +0000

    A little refactoring

---
+++ src/components/board/board.jsx
@@ -0,0 +1,35 @@
+import './board.scss';
+
+export const Board = (props) => {
+  return (
+    <div className='board'>
+      {props.guesses.length === 0 ? (
+        <div className='guess'>&nbsp;</div>
+      ) : (
+        props.guesses.map((guess, index) => {
+          const guessState =
+            guess === props.correctLetter ? 'correct' : 'incorrect';
+          return (
+            <div className={`guess ${guessState}`} key={index}>
+              {guess}
+            </div>
+          );
+        })
+      )}
+
+      <div ref={props.boardEndRef}>
+        {props.correctLetter.length === 1 && (
+          <div>
+            <h2>Congratulations!</h2>
+            <p>
+              You solved the <strong>Letterdle</strong> in{' '}
+              <strong className='correct-text'>{props.guesses.length}</strong>{' '}
+              guess
+              {props.guesses.length > 1 ? 'es' : ''}.
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
